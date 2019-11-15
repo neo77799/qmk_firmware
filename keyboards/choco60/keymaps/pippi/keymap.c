4abefffb09cd9aaabca262f099a6c58c0e8d121e
neo <tsuyoshi77799@yahoo.co.jp>
2019-11-15 10:37:10 +0900

    V5.1

--- keyboards/choco60/keymaps/pippi/keymap.c
+++ keyboards/choco60/keymaps/pippi/keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LCTL, KC_LALT, KC_SPACE,                         KC_FN, KC_SPACE,         KC_RALT,  KC_RCTL
   ),
   [_FN] = LAYOUT(
-    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,         KC_F6,     KC_F7,   KC_F8,    KC_F9,    KC_F10,    KC_F11,    KC_F12,    KC_INSERT, KC_DELETE,
+    RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,         KC_F6,     KC_F7,   KC_F8,    KC_F9,    KC_F10,    KC_F11,    KC_F12,    KC_INSERT, KC_DELETE,
     _______, _______, _______, _______, _______, _______,       _______,   KC_PSCR, KC_UP,    KC_SLCK,  _______,   KC_LBRC,   KC_RBRC,   KC_DELETE,
     KC_CAPS, _______, _______, _______, _______, _______,       KC_PGUP,   KC_LEFT, KC_DOWN,  KC_RIGHT, _______,   _______,   KC_ENTER,
     _______, _______, _______, _______, _______, _______,       KC_PGDOWN, KC_HOME, KC_END,   _______,  _______,              KC_RSHIFT, KC_FN,
